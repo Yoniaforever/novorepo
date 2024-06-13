14edc593073fcc2438f13a03165f842756e4e66f
Matheus <matheusmunhoz35@gmail.com>
2024-06-13 20:39:59 -0300

    fazendo a aula

---
+++ aula1.py
@@ -0,0 +1,12 @@
+"""
+O aluno matheus teve a primeira nota 2, a segunda nota 3 e a nota final 5
+
+"""
+
+nome = "matheus"
+n1 = 2
+n2 = 3
+n3 = 5
+soma = n1 + n2 + n3
+
+print ("o aluno" , nome , "teve a primeira nota" , n1 , "a segunda nota" , n2 , "e a nota final" , soma )
