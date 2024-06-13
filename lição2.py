5ee4bdca1f734ab1a835db0ddff85e20550b5070
Matheus <matheusmunhoz35@gmail.com>
2024-06-13 20:56:45 -0300

    ok

--- lição2.py
+++
@@ -1,8 +0,0 @@
-nome = "Paula"
-mês = "Janeiro"
-valor = 500
-desconto = "10%"
-cupom = "PAULAÉ10"
-
-
-print ("Olá," , nome , "Em" , mês , "você realizou uma compra no valor de" , valor , "e ganhou um desconto de" , desconto , "em sua próxima compra")
