260d59368fd68a6fb9760efdcbd8be70c769dd2a
Matheus <matheusmunhoz35@gmail.com>
2024-06-13 20:54:40 -0300

    vou chorarr

--- aula1.py
+++ aula1.py
@@ -1,7 +1,7 @@
-nome = "matheus"
-n1 = 2
-n2 = 3
-n3 = 5
-soma = n1 + n2 + n3
+nome = "Paula"
+mês = "Janeiro"
+valor = 500
+desconto = "10%"
+cupom = "PAULAÉ10"
 
-print ("o aluno" , nome , "teve a primeira nota" , n1 , "a segunda nota" , n2 , "e a nota final" , soma )
+print ("Olá," , nome , "Em" , mês , "você realizou uma compra no valor de" , valor , "e ganhou um desconto de" , desconto , "em sua próxima compra")
