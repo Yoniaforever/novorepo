c5be382993e1caf68d5e1657f559861932c34ab9
Matheus <matheusmunhoz35@gmail.com>
2024-06-17 18:49:01 -0300

    fiz a lição de casa

---
+++ atividadecasa.py
@@ -0,0 +1,8 @@
+nome = input("Por favor, digite seu nome de usuário: ")
+altura = input("Agora sua altura: ")
+idade = input("sua idade por favor")
+
+alt = float(altura)
+alt = int(idade)
+
+print(f"O nome de usuário da pessoa é {nome}, e sua altura é {altura} e sua idade é {idade}")
