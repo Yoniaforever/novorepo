162991bd38ef6f082e0d7f42a190677c4412cce8
Matheus <matheusmunhoz35@gmail.com>
2024-07-16 19:16:35 -0300

    banana

--- modulo02/atv_pesquisa.py
+++ modulo02/atv_pesquisa.py
@@ -2,7 +2,7 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-
+import time
 browser = Firefox()
 
 link = "https://google.com"
@@ -14,12 +14,27 @@ input_area = browser.find_element(By.NAME, "q")
 input_area.send_keys("instituto joga junto")
 input_area.send_keys(Keys.ENTER)
 
-result_search = browser.find_elements(By.NAME, "h3")
+time.sleep(5)
+
+result_search = browser.find_elements(By.TAG_NAME, 'h3')
 print (result_search)
 
-check = False
+
 for result in result_search:
     if "Instituto Joga Junto" in result.text:
         result.click()
-        check = True
+        break
+time.sleep(5)
+
+
+field_name = browser.find_element(By.NAME, 'name').send_keys('Matheus Munhoz')
+field_email = browser.find_element(By.NAME, 'email').send_keys('matheusmunhoz35@gmail.com')
+field_body = browser.find_element(By.NAME, 'body').send_keys('Minha primeira automação com selenium')
+
+field_subject = browser.find_element(By.NAME, 'assunto')
+select_subject = Select(field_subject)
+
+select_subject.select_by_visible_text('Ser facilitador')
+
+btn_send = browser.find_element(By.XPATH, '/html/body/div/div[2]/section[8]/div[1]/form/button').submit()
 
